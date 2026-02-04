4658b92bf19f0a3d6eca2d86b68ae676d03b138e
neophyte <neophyteplayground@gmail.com>
2026-02-04 03:27:01 +0000

    Revert "mm: Reduce max compact and reclaim retries"
    This reverts commit 99a4aefb2eb1370f8812577ce00d4206e26ce097.
    
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- mm/internal.h
+++ mm/internal.h
@@ -105,7 +105,7 @@ extern unsigned long highest_memmap_pfn;
  * Maximum number of reclaim retries without progress before the OOM
  * killer is consider the only way forward.
  */
-#define MAX_RECLAIM_RETRIES 6
+#define MAX_RECLAIM_RETRIES 16
 
 /*
  * in mm/vmscan.c:
